ac5855008ed168e9531884dd3326df59bcea1ed8
lorenzoriano <lorenzo.riano@berkeley.edu>
2012-10-09 17:26:33 -0700

    first test for generating collision-free poses

---
+++ test-collisions.py
@@ -0,0 +1,94 @@
+import openravepy
+import time
+import numpy as np
+
+def make_orth_basis(x_ax):
+    """
+    John Schulman magic code.
+    """
+    x_ax = np.asarray(x_ax)
+
+    x_ax = x_ax / np.linalg.norm(x_ax)
+    if np.allclose(x_ax, [1,0,0]):
+        return np.eye(3)
+    elif np.allclose(x_ax, [-1, 0, 0]):
+        return np.array([
+            [-1, 0, 0],
+            [0, -1, 0],
+            [0, 0, 1]])
+    else:
+        y_ax = np.r_[0, x_ax[2], -x_ax[1]]
+        y_ax /= np.linalg.norm(y_ax)
+        z_ax = np.cross(x_ax, y_ax)
+        return np.c_[x_ax, y_ax, z_ax]
+
+def generate_random_pos(robot):
+    max_x = 2.5
+    max_y = 2.5
+    max_th = np.pi
+    
+    T = robot.GetTransform()
+    x = np.random.uniform(-max_x, max_x)
+    y = np.random.uniform(-max_y, max_y)
+    z = T[2,3]
+    
+    th = np.random.uniform(-max_th, max_th)
+    
+    #rotation
+    T = openravepy.matrixFromAxisAngle([0,0,th])
+    #translation
+    T[:, -1] = [x, y, z, 1]    
+    return T
+
+def check_reachable(manip, obj):
+    
+    #all of this is to get the angles between the manipulator and the object
+    obj_pos = obj.GetTransform()[:3,-1]
+    manip_pos = manip.GetTransform()[:3,-1]
+    openravepy.raveLogInfo("Manipulator position: " + str(manip_pos))
+    
+    rot_mat = make_orth_basis(obj_pos - manip_pos)
+    T = np.eye(4)
+    T[:3, :-1] = rot_mat
+    T[:3,-1] = obj_pos
+    sol = manip.FindIKSolution(T, openravepy.IkFilterOptions.IgnoreEndEffectorCollisions)
+    return sol
+    
+
+def main():
+    env = openravepy.Environment()
+    env.SetViewer('qtcoin')
+    env.Load('data/pr2test1.env.xml')
+    robot=env.GetRobots()[0]
+    mug =[b for b in env.GetBodies() if b.GetName() == 'mug1'][0]
+    
+    manip = robot.SetActiveManipulator('leftarm_torso')
+    ikmodel = openravepy.databases.inversekinematics.InverseKinematicsModel(
+        robot,iktype=openravepy.IkParameterization.Type.Transform6D)
+    if not ikmodel.load():
+        ikmodel.autogenerate()
+    
+    num_contacts = 1 #just cheating
+    isreachable = False
+    while (num_contacts > 0) or (not isreachable):
+        T = generate_random_pos(robot)    
+        robot.SetTransform(T)    
+        env.GetCollisionChecker().SetCollisionOptions(openravepy.CollisionOptions.Contacts)
+            
+        # get first collision
+        report = openravepy.CollisionReport()
+        collision=env.CheckCollision(robot,report=report)
+        num_contacts = len(report.contacts)
+        openravepy.raveLogInfo("Number of contacts: " + str(num_contacts))
+        sol = check_reachable(manip, mug)
+        isreachable = sol is not None
+        #time.sleep(1)
+    
+    robot.SetDOFValues(sol, manip.GetArmIndices())
+    openravepy.raveLogInfo("Done!!")
+
+if __name__ == "__main__":
+    main()
+    while True:
+        time.sleep(0.1)
+    
