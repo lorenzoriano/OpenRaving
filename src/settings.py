e8ef98efe8353bc3c698e550762808f97f6f1ddb
lorenzoriano <lorenzo.riano@berkeley.edu>
2013-01-16 11:50:49 -0800

    new global parameters

--- src/settings.py
+++ src/settings.py
@@ -8,3 +8,7 @@ if pddlDomainFile == path:
 initialProblemFile = path #+ raw_input("Enter PDDL problem file name: ")
 if initialProblemFile == path:
     initialProblemFile = path + "robotics_autogen_prob.pddl"
+
+
+collision_free_grasping_samples = 500
+occluding_objects_grasping_samples = 100
