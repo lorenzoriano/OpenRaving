f5d671e625437e13ae29fd218b9345b5219fd445
siddharth <siddharth@monk.(none)>
2013-01-17 16:09:37 -0800

    minor edit

--- src/settings.py
+++ src/settings.py
@@ -4,7 +4,7 @@ errFileName = "robotics_autogen_err1.txt"
 ff = "../planners/FF-v2.3/ff"
 pddlDomainFile = path #+ raw_input("Enter PDDL domain file name: ")
 if pddlDomainFile == path:
-    pddlDomainFile = path+ "robotics_obstrn_compiled_dom_35objs.pddl"
+    pddlDomainFile = path+ "robotics_obstrn_compiled_dom2.pddl"
 initialProblemFile = path #+ raw_input("Enter PDDL problem file name: ")
 if initialProblemFile == path:
     initialProblemFile = path + "robotics_autogen_prob.pddl"
@@ -12,4 +12,4 @@ if initialProblemFile == path:
 
 collision_free_grasping_samples = 500
 occluding_objects_grasping_samples = 200
-envFile = "boxes.dae"
+envFile = "boxes50.dae"
