629405aca3ac8289963f54fe34c864dd6f7085b3
siddharth <siddharth@Thor.(none)>
2013-01-20 18:44:17 -0800

    domain and problem files for optimal planner (commented)

--- src/settings.py
+++ src/settings.py
@@ -6,12 +6,16 @@ fd = "../planners/FD/src/plan-ipc seq-sat-fd-autotune-1 "
 pddlDomainFile = path #+ raw_input("Enter PDDL domain file name: ")
 if pddlDomainFile == path:
 #    pddlDomainFile = path+ "robotics_obstrn_compiled_dom2.pddl"
+     # for ff, dinnertime:
      pddlDomainFile = path+ "dinnerTime_dom.pddl"
-    
+     #pddlDomainFile = path+ "dinnerTimeNoNegationCosts_dom.pddl"
+
 initialProblemFile = path #+ raw_input("Enter PDDL problem file name: ")
 if initialProblemFile == path:
 #    initialProblemFile = path + "robotics_autogen_prob.pddl"
+    #for ff, dinnertime:
     initialProblemFile = path + "dinnerTime_prob.pddl"
+    #initialProblemFile = path + "dinnerTimeNoNegationCosts_prob.pddl"
 
 
 collision_free_grasping_samples = 500
