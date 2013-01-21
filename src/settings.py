13790e7ddec247570615c865a829e5ddb207def0
siddharth <siddharth@Thor.(none)>
2013-01-20 22:27:09 -0800

    defaults for boxes

--- src/settings.py
+++ src/settings.py
@@ -5,23 +5,23 @@ ff = "../planners/FF-v2.3/ff"
 fd = "../planners/FD/src/plan-ipc seq-sat-fd-autotune-1 "
 pddlDomainFile = path #+ raw_input("Enter PDDL domain file name: ")
 if pddlDomainFile == path:
-#    pddlDomainFile = path+ "robotics_obstrn_compiled_dom2.pddl"
+    pddlDomainFile = path+ "robotics_obstrn_compiled_dom2.pddl"
      # for ff, dinnertime:
      #pddlDomainFile = path+ "dinnerTime_dom.pddl"   
-     pddlDomainFile = path+ "dinnerTimeNoNegation_dom.pddl"
+     #pddlDomainFile = path+ "dinnerTimeNoNegation_dom.pddl"
     #pddlDomainFile = path+ "dinnerTimeNoNegationCosts_dom.pddl"
 
 initialProblemFile = path #+ raw_input("Enter PDDL problem file name: ")
 if initialProblemFile == path:
-#    initialProblemFile = path + "robotics_autogen_prob.pddl"
+    initialProblemFile = path + "robotics_autogen_prob.pddl"
     #for ff, dinnertime:
     #initialProblemFile = path + "dinnerTime_prob.pddl"
-    initialProblemFile = path + "dinnerTimeNoNegation_prob.pddl"
+    #initialProblemFile = path + "dinnerTimeNoNegation_prob.pddl"
 
     #initialProblemFile = path + "dinnerTimeNoNegationCosts_prob.pddl"
 
 
 collision_free_grasping_samples = 500
 occluding_objects_grasping_samples = 200
-#envFile = "boxes50.dae"
-envFile = "tray_world.dae"
+envFile = "boxes100.dae"
+#envFile = "tray_world.dae"
