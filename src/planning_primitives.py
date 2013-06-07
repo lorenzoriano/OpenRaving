8feb73b9483d727ca3c20faef825785fb3d19f44
Eugene Fang <fang.eugene@gmail.com>
2013-06-06 17:09:45 -0700

    remove hackish way of choosing object

--- src/planning_primitives.py
+++ src/planning_primitives.py
@@ -100,10 +100,6 @@ class Executor(object):
         print "ignore me !"    
     
     def grasp(self, obj_name, _unused1, _unused2):
-        # NOTICE: Manual selection of objects is used for debugging purposes only
-        num = raw_input("Enter an object number, or press Enter to continue: ")
-        if num.isdigit():
-            obj_name = 'object{}'.format(num)
 
         print "Grasping object ", obj_name
         obj = self.env.GetKinBody(obj_name)
