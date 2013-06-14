5cfe8a381039607bf1583423733ce9a7bed1b56f
Eugene Fang <fang.eugene@gmail.com>
2013-06-14 16:26:54 -0700

    fix DOF limit issue

--- src/trajectory_generator.py
+++ src/trajectory_generator.py
@@ -11,120 +11,133 @@ class TrajectoryGenerator(object):
     self.manip = self.robot.GetActiveManipulator()
     self.n_steps = n_steps
     self.collision_checker = CollisionChecker(self.env)
+    # self.viewer = trajoptpy.GetViewer(self.env)
+    # trajoptpy.SetInteractive(True)
+    self.lower,self.upper = self.robot.GetDOFLimits()
+    self.lower -= .3
+    self.upper += .3
 
   def generate_traj(self, pos, rot,
                     init_joints=None,
                     collisionfree=True,
                     n_steps=None):
-    if init_joints is None:
-      mat = openravepy.matrixFromPose(rot + pos)
+    with self.env:
+      self.robot.SetDOFLimits(self.lower, self.upper)
 
-      if collisionfree:
-        init_joints = self.manip.FindIKSolution(mat,
-          openravepy.IkFilterOptions.CheckEnvCollisions)
-      else:
-        init_joints = self.manip.FindIKSolution(mat,
-          openravepy.IkFilterOptions.IgnoreEndEffectorCollisions)
+      if init_joints is None:
+        mat = openravepy.matrixFromPose(rot + pos)
 
-    request = {
-      "basic_info" : {
-        "n_steps" : self.n_steps if (n_steps is None) else n_steps,
-        "manip" : "rightarm", # see below for valid values
-        "start_fixed" : True # i.e., DOF values at first timestep are fixed based on current robot state
-      },
-      "costs" : [
-      {
-        "type" : "joint_vel", # joint-space velocity cost
-        "params": {"coeffs" : [50]} # a list of length one is automatically expanded to a list of length n_dofs
-        # Also valid: "coeffs" : [7,6,5,4,3,2,1]
-      },
-      {
-        "type" : "collision",
-        "name" : "col", # Shorten name so printed table will be prettier
-        "params" : {
-          "coeffs" : [40], # penalty coefficients. list of length one is automatically expanded to a list of length n_timesteps
-          "dist_pen" : [0] # robot-obstacle distance that penalty kicks in. expands to length n_timesteps
+        if collisionfree:
+          init_joints = self.manip.FindIKSolution(mat,
+            openravepy.IkFilterOptions.CheckEnvCollisions)
+        else:
+          init_joints = self.manip.FindIKSolution(mat,
+            openravepy.IkFilterOptions.IgnoreEndEffectorCollisions)
+
+      request = {
+        "basic_info" : {
+          "n_steps" : self.n_steps if (n_steps is None) else n_steps,
+          "manip" : "rightarm", # see below for valid values
+          "start_fixed" : True # i.e., DOF values at first timestep are fixed based on current robot state
+        },
+        "costs" : [
+        {
+          "type" : "joint_vel", # joint-space velocity cost
+          "params": {"coeffs" : [50]} # a list of length one is automatically expanded to a list of length n_dofs
+          # Also valid: "coeffs" : [7,6,5,4,3,2,1]
+        },
+        {
+          "type" : "collision",
+          "name" : "col", # Shorten name so printed table will be prettier
+          "params" : {
+            "continuous":False,
+            "coeffs" : [40], # penalty coefficients. list of length one is automatically expanded to a list of length n_timesteps
+            "dist_pen" : [0] # robot-obstacle distance that penalty kicks in. expands to length n_timesteps
+          }
+        },
+        {
+          "type" : "collision",
+          "name" : "cont_col", # Shorten name so printed table will be prettier
+          "params" : {
+            "coeffs" : [40], # penalty coefficients. list of length one is automatically expanded to a list of length n_timesteps
+            "dist_pen" : [0.025] # robot-obstacle distance that penalty kicks in. expands to length n_timesteps
+          }
+        }],
+        "constraints" : [
+        {
+          "type" : "pose",
+          "params" : {"xyz" : pos,
+                      "wxyz" : rot,
+                      "link": "r_gripper_tool_frame",
+                      "pos_coeffs" : [20, 20, 20],
+                      "rot_coeffs" : [20, 20, 20]}
+        }],
+        "init_info" : {
+            "type" : "straight_line", # straight line in joint space.
+            "endpoint" : init_joints.tolist()
         }
-      },
-      {
-        "type" : "continuous_collision",
-        "name" : "cont_col", # Shorten name so printed table will be prettier
-        "params" : {
-          "coeffs" : [40], # penalty coefficients. list of length one is automatically expanded to a list of length n_timesteps
-          "dist_pen" : [0.025] # robot-obstacle distance that penalty kicks in. expands to length n_timesteps
-        }
-      }],
-      "constraints" : [
-      {
-        "type" : "pose",
-        "params" : {"xyz" : pos,
-                    "wxyz" : rot,
-                    "link": "r_gripper_tool_frame",
-                    "pos_coeffs" : [20, 20, 20],
-                    "rot_coeffs" : [20, 20, 20]}
-      }],
-      "init_info" : {
-          "type" : "straight_line", # straight line in joint space.
-          "endpoint" : init_joints.tolist()
       }
-    }
 
-    prob = trajoptpy.ConstructProblem(json.dumps(request), self.env)
+      prob = trajoptpy.ConstructProblem(json.dumps(request), self.env)
 
-    result = trajoptpy.OptimizeProblem(prob)
-    traj = result.GetTraj()
+      result = trajoptpy.OptimizeProblem(prob)
+      traj = result.GetTraj()
 
-    collisions = self.collision_checker.get_collisions(traj)
-    if collisionfree and collisions:
-      return None, collisions
+      collisions = self.collision_checker.get_collisions(traj)
+      if collisionfree and collisions:
+        return None, collisions
 
-    return traj, collisions
+      return traj, collisions
 
   def generate_traj_with_joints(self, joint_target, n_steps=None):
-    request = {
-      "basic_info" : {
-        "n_steps" : self.n_steps if (n_steps is None) else n_steps,
-        "manip" : "rightarm", # see below for valid values
-        "start_fixed" : True # i.e., DOF values at first timestep are fixed based on current robot state
-      },
-      "costs" : [
-      {
-        "type" : "joint_vel", # joint-space velocity cost
-        "params": {"coeffs" : [10]} # a list of length one is automatically expanded to a list of length n_dofs
-        # Also valid: "coeffs" : [7,6,5,4,3,2,1]
-      },
-      {
-        "type" : "collision",
-        "name" : "col", # Shorten name so printed table will be prettier
-        "params" : {
-          "coeffs" : [20], # penalty coefficients. list of length one is automatically expanded to a list of length n_timesteps
-          "dist_pen" : [0.00] # robot-obstacle distance that penalty kicks in. expands to length n_timesteps
-        }
-      },
-      {
-        "type" : "continuous_collision",
-        "name" : "cont_col", # Shorten name so printed table will be prettier
-        "params" : {
-          "coeffs" : [20], # penalty coefficients. list of length one is automatically expanded to a list of length n_timesteps
-          "dist_pen" : [0.025] # robot-obstacle distance that penalty kicks in. expands to length n_timesteps
-        }
-      }],
-      "constraints" : [
-      {
-        "type" : "joint", # joint-space target
-        "params" : {"vals" : joint_target } # length of vals = # dofs of manip
-      }
-      ],
-      "init_info" : {
-          "type" : "straight_line", # straight line in joint space.
-          "endpoint" : joint_target
-      }
-    }
+    with self.env:
+      self.robot.SetDOFLimits(self.lower, self.upper)
 
-    prob = trajoptpy.ConstructProblem(json.dumps(request), self.env)
-    result = trajoptpy.OptimizeProblem(prob)
-    traj = result.GetTraj()
-    return traj
+      request = {
+        "basic_info" : {
+          "n_steps" : self.n_steps if (n_steps is None) else n_steps,
+          "manip" : "rightarm", # see below for valid values
+          "start_fixed" : True # i.e., DOF values at first timestep are fixed based on current robot state
+        },
+        "costs" : [
+        {
+          "type" : "joint_vel", # joint-space velocity cost
+          "params": {"coeffs" : [10]} # a list of length one is automatically expanded to a list of length n_dofs
+          # Also valid: "coeffs" : [7,6,5,4,3,2,1]
+        },
+        {
+          "type" : "collision",
+          "name" : "col", # Shorten name so printed table will be prettier
+          "params" : {
+            "continuous": False,
+            "coeffs" : [20], # penalty coefficients. list of length one is automatically expanded to a list of length n_timesteps
+            "dist_pen" : [0.00] # robot-obstacle distance that penalty kicks in. expands to length n_timesteps
+          }
+        },
+        {
+          "type" : "collision",
+          "name" : "cont_col", # Shorten name so printed table will be prettier
+          "params" : {
+            "coeffs" : [20], # penalty coefficients. list of length one is automatically expanded to a list of length n_timesteps
+            "dist_pen" : [0.025] # robot-obstacle distance that penalty kicks in. expands to length n_timesteps
+          }
+        }],
+        "constraints" : [
+        {
+          "type" : "joint", # joint-space target
+          "params" : {"vals" : joint_target } # length of vals = # dofs of manip
+        }
+        ],
+        "init_info" : {
+            "type" : "straight_line", # straight line in joint space.
+            "endpoint" : joint_target
+        }
+      }
+
+      prob = trajoptpy.ConstructProblem(json.dumps(request), self.env)
+      result = trajoptpy.OptimizeProblem(prob)
+      traj = result.GetTraj()
+      return traj
 
 class GraspTrajectoryGenerator(object):
   def __init__(self, env, unmovable_objects=set()):
